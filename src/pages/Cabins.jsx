d758462fdd09e1a94a144e2fea33d15688e033ca
Paul Maxwell <165905426+pmaxunlimited@users.noreply.github.com>
2024-06-22 15:06:05 -0400

    Add react-hook-form/supabase integration

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,18 +1,28 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable";
+import { useQuery } from "@tanstack/react-query";
 import { getCabins } from "../services/apiCabins";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import { useState } from "react";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
+  const [showForm, setShowForm] = useState(false);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
+      </Row>
+    </>
   );
 }
 
