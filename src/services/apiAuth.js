11edb42f7b78c255e24dd621cd50ef51a3aeb3fd
Paul Maxwell <165905426+pmaxunlimited@users.noreply.github.com>
2024-07-04 07:56:57 -0400

    Add user signup

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,22 @@
 import supabase from "./supabase";
 
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
+
 export async function login({ email, password }) {
   let { data, error } = await supabase.auth.signInWithPassword({
     email: email,
@@ -7,7 +24,7 @@ export async function login({ email, password }) {
   });
 
   if (error) throw new Error(error.message);
-  console.log(data);
+
   return data;
 }
 
@@ -16,8 +33,13 @@ export async function getCurrentUser() {
   if (!session.session) return null;
 
   const { data, error } = await supabase.auth.getUser();
-  console.log(data);
+
   if (error) throw new Error(error.message);
 
   return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
